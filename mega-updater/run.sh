db860eccd2de7d760933fb6241e94e014c60703e
Andrey <andvikt@gmail.com>
2021-03-05 10:01:19 +0300

    new version

--- mega-updater/run.sh
+++ mega-updater/run.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bashio
+set -e
+
+echo "download latest update script from $MEGAD_SCRIPT_URL"
+curl -L -f -s "$MEGAD_SCRIPT_URL" > megad.zip
+unzip megad.zip
+rm megad.zip
+
 CONF=$(bashio::config 'hosts')
 localip=$(bashio::config 'localip')
+options=$(bashio::config 'options') || true
+
+if [ -z "$options" ]
+then
+  options="-w --ee --read-conf /backup/megad-$ip.cfg --write-conf /backup/megad-$ip.cfg"
+fi
 
 for cfg in $CONF
 do
   ip=$(bashio::jq "$cfg" '.ip')
   password=$(bashio::jq "$cfg" '.password')
-  cmd="php megad-cfg-2561.php --ip $ip -p $password -w --ee --local-ip $localip --read-conf megad.cfg --write-conf megad.cfg"
+  cmd="php megad-cfg-2561.php --ip $ip -p $password --local-ip $localip $options"
   echo "$cmd"
   $cmd
 done
