207e7d72a6e58c9105cf2f605e279b1d77f98a1a
andvikt <andvikt@gmail.com>
2021-08-29 10:21:48 +0300

    fix arm support

--- configconv.py
+++ configconv.py
@@ -3,7 +3,8 @@ import json
 import yaml
 
 if __name__ == '__main__':
-    with open('config.yml') as f:
-        cfg = yaml.load(f, Loader=yaml.CLoader)
-    with open('config.json', 'w') as f:
-        json.dump(cfg, f)
+    for n in ['config', 'build']:
+        with open(f'{n}.yml') as f:
+            cfg = yaml.load(f, Loader=yaml.CLoader)
+        with open(f'{n}.json', 'w') as f:
+            json.dump(cfg, f)
